c2cd8307941448022f6a83407c2c242b9b917a09
Your Name <olegglotov99@gmail.com>
2021-10-17 01:03:12 -0700

    lab1work

--- 2DA4 - Lab 1/lab1.v
+++ 2DA4 - Lab 1/lab1.v
@@ -1,17 +1,19 @@
 
+//PSEUDO CODE
+/*
 module dual2(x1, x0, y1, y0, f2, f1);
 
-// ===== INPUT/OUTPUT DECLARATIONS ===== //
+	// ===== INPUT/OUTPUT DECLARATIONS ===== //
 
-input x1, x0, y1, y0;
-output f1, f2;
+	input x1, x0, y1, y0;
+	output f1, f2;
 
-assign f1 = (~x1 &     	  y1      ) |
-				(      ~x0 &        y0) |
-				( x1       & ~y1      ) |
-				(       x0 &       ~y0) ;
+	assign f1 = (~x1 &  y1) |
+				(~x0 &  y0) |
+				( x1 & ~y1) |
+				( x0 & ~y0) ;
 
-assign f2 = (~x1 & ~x0 & ~y1 & ~y0) |
+	assign f2 = (~x1 & ~x0 & ~y1 & ~y0) |
 				(~x1 &  x0 & ~y1 & ~y0) |
 				(~x1 &  x0 & ~y1 &  y0) |
 				( x1 & ~x0 & ~y1 & ~y0) |
@@ -21,6 +23,57 @@ assign f2 = (~x1 & ~x0 & ~y1 & ~y0) |
 				( x1 &  x0 & ~y1 &  y0) |
 				( x1 &  x0 &  y1 & ~y0) |
 				( x1 &  x0 &  y1 &  y0) ;
+
+endmodule
+*/
+
+//updated version
+
+/*
+
+module dual2(SW[9], SW[8], SW[7], SW[6], LEDR[8], LEDR[9]);
+
+// ===== Input/Output Declarations ===== //
+	input SW[9], SW[8], SW[7], SW[6];
+	output LEDR[8],LEDR[9];
+
+// ===== Net/Reg Declarations ===== //
+   //reg [9:8]LEDR;
+
+// ===== Logic ===== //
+	assign LEDR[9] = (~SW[9] &  SW[7]) |
+				(~SW[8] &  SW[6]) |
+				( SW[9] & ~SW[7]) |
+				( SW[8] & ~SW[6]) ;
+
+	assign LEDR[8] = (~SW[9] & ~SW[8] & ~SW[7] & ~SW[6]) |
+				(~SW[9] &  SW[8] & ~SW[7] & ~SW[6]) |
+				(~SW[9] &  SW[8] & ~SW[7] &  SW[6]) |
+				( SW[9] & ~SW[8] & ~SW[7] & ~SW[6]) |
+				( SW[9] & ~SW[8] & ~SW[7] &  SW[6]) |
+				( SW[9] & ~SW[8] &  SW[7] & ~SW[6]) |
+				( SW[9] &  SW[8] & ~SW[7] & ~SW[6]) |
+				( SW[9] &  SW[8] & ~SW[7] &  SW[6]) |
+				( SW[9] &  SW[8] &  SW[7] & ~SW[6]) |
+				( SW[9] &  SW[8] &  SW[7] &  SW[6]) ;
+endmodule
+
+*/
+
+module dual2(SW[9], SW[8], SW[7], SW[6], LEDR[9]);
+
+// ===== Input/Output Declarations ===== //
+	input SW[9], SW[8], SW[7], SW[6];
+	output LEDR[9];
+
+// ===== Net/Reg Declarations ===== //
+   //reg [9:8]LEDR;
+
+// ===== Logic ===== //
+	assign LEDR[9] = (~SW[9] &  SW[7]) |
+				(~SW[8] &  SW[6]) |
+				( SW[9] & ~SW[7]) |
+				( SW[8] & ~SW[6]) ;
 
 endmodule
 
