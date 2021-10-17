cc49e902cbc136d52d555f3f8984711ebdff43f3
OlegGlo <olegglotov99@gmail.com>
2021-10-17 19:18:36 -0400

    part 2 integrated

--- 2DA4 - Lab 2/part2/lab2part2.v
+++ 2DA4 - Lab 2/part2/lab2part2.v
@@ -1,4 +1,4 @@
-
+/*
 module part2(x3, x2, x1, x0, s0, s1, s2, s3, s4, s5, s6);
     // ===== Input/Output Declarations ===== //
 	input x3, x2, x1, x0;
@@ -28,8 +28,56 @@ module part2(x3, x2, x1, x0, s0, s1, s2, s3, s4, s5, s6);
                 (!x2 &  x0) |
                 (!x1 &  x0) ;
 endmodule
+*/
 
-    // ===== Output generated =====
-    // OLEG HIBPC or close to it
+// ===== Output generated =====
+// OLEG HIBPC or close to it
 
-    
+//use HEX4[6:0] and SW[4:1]
+// HEX4[0] = s0
+// HEX4[1] = s1
+// HEX4[2] = s2
+// HEX4[3] = s3
+// HEX4[4] = s4
+// HEX4[5] = s5
+// HEX4[6] = s6
+
+// SW[4] = x3
+// SW[3] = x2
+// SW[2] = x1
+// SW[1] = x0
+
+module part2(SW, HEX4);
+
+// ===== Input/Output Declarations ===== //
+
+	//input SW[4], SW[3], SW[2], SW[1];
+    input wire [4:1]SW;
+	//output HEX4[0], HEX4[1], HEX4[2], HEX4[3], HEX4[4], HEX4[5], HEX4[6];
+    output wire [6:0]HEX4;
+
+// ===== Logic ===== //
+
+	assign HEX4[0] = (!SW[2] &  SW[1]) |  
+                    ( SW[3] & !SW[2]) ;
+
+	assign HEX4[1] = (SW[4]) | 
+                    (!SW[2] &  SW[1]) |  
+                    (!SW[3] &  SW[2]) ;
+
+	assign HEX4[2] = (SW[4]) | 
+                    (!SW[2] &  SW[1]) |  
+                    ( SW[3] &  SW[1]) |
+                    (!SW[3] & SW[2] & !SW[1]);
+
+	assign HEX4[3] = ( SW[3] & !SW[2]) |  
+                    ( SW[3] &  SW[1]) ;
+
+	assign HEX4[4] = 0; //always low - can also do HEX4[5] <= 1'b0;
+
+	assign HEX4[5] = 0;
+
+	assign HEX4[6] = (!SW[3] & !SW[2]) |  
+                    (!SW[3] &  SW[1]) |
+                    (!SW[2] &  SW[1]) ;
+endmodule
