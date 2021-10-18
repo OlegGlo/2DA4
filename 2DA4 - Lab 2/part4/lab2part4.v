487eea703ac944164ac6d118a380b7bc059bdbbd
OlegGlo <olegglotov99@gmail.com>
2021-10-17 21:24:47 -0400

    part4

--- 2DA4 - Lab 2/part4/lab2part4.v
+++ 2DA4 - Lab 2/part4/lab2part4.v
@@ -1,17 +1,31 @@
-module part4(D, CLOCK, Q);
+module part4(Q, SW, KEY);
 
 // == Input and Output ==
 
-	input D, CLOCK;
+	
+	input [0:0] KEY;
+	input [9:9] SW;
+
 	output Q;
+	output wire HEX5[6:0];
 	
 // == Net/Reg ==
 
-	output reg Q;
+	reg Q;
 	
 // == Logic ==
 	
-	always @(posedge CLOCK)
-		Q <= D;
+	always @(posedge KEY)
+		if (~KEY)
+			Q <= SW[9];
+			if(Q == 0)
+				HEX5[5:0] <= {6[1'b0]}
+			else
+				HEX5[2:1] <= {2[1'b1]}
+		else
+			if(Q == 0)
+				HEX5[5:0] <= {6[1'b0]}
+			else
+				HEX5[2:1] <= {2[1'b1]}
 
 endmodule 
