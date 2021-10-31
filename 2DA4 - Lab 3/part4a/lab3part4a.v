bd6c96c2bc2f4888141c8db96c84ab8fa2277fe6
OlegGlo <olegglotov99@gmail.com>
2021-10-31 18:22:30 -0400

    test

--- 2DA4 - Lab 3/part4a/lab3part4a.v
+++ 2DA4 - Lab 3/part4a/lab3part4a.v
@@ -1,14 +1,16 @@
-module seg7(hex,leds);
-	input [3:0] hex;
+module seg7(HEX5,SW);//hex, leds
+
+	//input [3:0] hex; this is the actual input, can change to wire  == keep as switches 0 to 3 for now
+	input [3:0] SW;
 	
-	output reg [1:7] leds;
-	
-	always @(hex)
-		case (hex)
-			0: leds = 7'b1111110;
-			1: leds = 7'b0110000;
+	//output reg [1:7] leds;
+	output [6:0] HEX5;
+
+	always @(SW)
+		case (SW)
+			0000: leds = 7'b1111110;
+			1111: leds = 7'b0110000;
 			//etc
-			
 		endcase
-		
 endmodule
+
